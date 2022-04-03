e8c49ab7236a3a8e20c5867e1929ee7630fb7463
ri-yad-a <riyadabdullayev04@gmail.com>
2022-04-02 19:30:19 -0600

    Reverted main back to original

--- src/main/java/com/main/locationary/Main.java
+++ src/main/java/com/main/locationary/Main.java
@@ -3,13 +3,9 @@ package com.main.locationary;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * CPSC 233 W22 Demo 3
@@ -19,22 +15,10 @@ import java.util.List;
  */
 
 public class Main extends Application {
-
-    static AnchorPane root;
-    static List<GridPane> grid = new ArrayList<GridPane>();
-
-    private static int idx = 0;
-
     @Override
     public void start(Stage stage) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("home-view.fxml"));
-
-        grid.add(FXMLLoader.load(getClass().getResource("bucketList-view.fxml")));
-        grid.add(FXMLLoader.load(getClass().getResource("visited-view.fxml")));
-
-        root.getChildren().add(grid.get(0));
-
-        Scene scene = new Scene(root, 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Locationary - Demo 3 Release!");
         stage.setScene(scene);
         stage.show();
