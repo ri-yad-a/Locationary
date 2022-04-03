a7f576738cbcd21dc81917aa1c1118c511b1a62b
ri-yad-a <riyadabdullayev04@gmail.com>
2022-04-02 21:47:54 -0600

    Started implementing the functionality of adding locations in bucketList and visited

--- src/main/java/com/main/locationary/BucketListController.java
+++ src/main/java/com/main/locationary/BucketListController.java
@@ -8,6 +8,8 @@ import javafx.scene.control.TextField;
 
 public class BucketListController {
 
+    BucketList bucketList = new BucketList();
+
     @FXML
     private TextField POITextField;
 
@@ -21,10 +23,38 @@ public class BucketListController {
     private TextField locationNameTextField;
 
     @FXML
-    private ChoiceBox<?> scopeChoiceBox;
+    private ChoiceBox<String> scopeChoiceBox;
+
+    @FXML
+    void initialize() {
+        // add monster weapon types
+        scopeChoiceBox.getItems().add("Citywide");
+        scopeChoiceBox.getItems().add("Domestic");
+        scopeChoiceBox.getItems().add("International");
+
+    }
 
     @FXML
     void addLocationButtonClicked(ActionEvent event) {
+
+        // get the user inputted location name
+        String locationName = locationNameTextField.getText();
+        // declare a location object that will be added to bucketList
+        Location bucketListLocation = null;
+
+        // check the scope choice of the user form scopeChoiceBox and set the visitedLocation object
+        // with the correct scope and location name
+        if (scopeChoiceBox.getValue() == "Citywide") {
+            bucketListLocation = new Location(Location.Scope.CITYWIDE, locationName);
+        } else if (scopeChoiceBox.getValue() == "Domestic") {
+            bucketListLocation = new Location(Location.Scope.DOMESTIC, locationName);
+        } else if (scopeChoiceBox.getValue() == "International") {
+            bucketListLocation = new Location(Location.Scope.INTERNATIONAL, locationName);
+        }
+
+        bucketList.addLocation(bucketListLocation);
+
+        // update bucketList display
 
     }
 
--- src/main/java/com/main/locationary/VisitedController.java
+++ src/main/java/com/main/locationary/VisitedController.java
@@ -2,10 +2,22 @@ package com.main.locationary;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 public class VisitedController {
+
+    Visited visited = new Visited();
+
+    @FXML
+    private CheckBox citywideCheckBox;
+
+    @FXML
+    private CheckBox domesticCheckBox;
+
+    @FXML
+    private CheckBox internationalCheckBox;
 
     @FXML
     private TextField locationNameTextField;
@@ -14,11 +26,38 @@ public class VisitedController {
     private TextField ratingTextField;
 
     @FXML
-    private ChoiceBox<?> scopeChoiceBox;
+    private ChoiceBox<String> scopeChoiceBox;
+
+    @FXML
+    void initialize() {
+        // add monster weapon types
+        scopeChoiceBox.getItems().add("Citywide");
+        scopeChoiceBox.getItems().add("Domestic");
+        scopeChoiceBox.getItems().add("International");
+
+    }
 
     @FXML
     void addLocationButtonClicked(ActionEvent event) {
 
+        // get the user inputted location name
+        String locationName = locationNameTextField.getText();
+        // declare a location object that will be added to visited
+        Location visitedLocation = null;
+
+        // check the scope choice of the user form scopeChoiceBox and set the visitedLocation object
+        // with the correct scope and location name
+        if (scopeChoiceBox.getValue() == "Citywide") {
+            visitedLocation = new Location(Location.Scope.CITYWIDE, locationName);
+        } else if (scopeChoiceBox.getValue() == "Domestic") {
+            visitedLocation = new Location(Location.Scope.DOMESTIC, locationName);
+        } else if (scopeChoiceBox.getValue() == "International") {
+            visitedLocation = new Location(Location.Scope.INTERNATIONAL, locationName);
+        }
+
+        visited.addLocation(visitedLocation);
+
+        // update visited display
     }
 
 }
