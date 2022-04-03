7542435b30a752146b8a9e367fff5e11ad264385
ri-yad-a <riyadabdullayev04@gmail.com>
2022-04-03 12:28:10 -0600

    Implemented functionality of radio buttons when viewing Visited, so that you can view citywide, domestic, international locations in any combination desired

--- src/main/java/com/main/locationary/VisitedController.java
+++ src/main/java/com/main/locationary/VisitedController.java
@@ -2,10 +2,7 @@ package com.main.locationary;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class VisitedController {
 
@@ -33,7 +30,20 @@ public class VisitedController {
     private TextField ratingTextField;
 
     @FXML
+    private ListView visitedListView;
+
+    @FXML
     private ChoiceBox<String> scopeChoiceBox;
+
+    @FXML
+    private RadioButton citywideRadioButton;
+
+    @FXML
+    private RadioButton domesticRadioButton;
+
+    @FXML
+    private RadioButton internationalRadioButton;
+
 
     @FXML
     void initialize() {
@@ -74,11 +84,64 @@ public class VisitedController {
         visited.addLocation(visitedLocation);
 
         // update visited display
+        if (visitedLocation.getScope() == Location.Scope.CITYWIDE) {
+            visitedListView.getItems().add("[C] " +visitedLocation);
+        } else if (visitedLocation.getScope() == Location.Scope.DOMESTIC) {
+            visitedListView.getItems().add("[D] " +visitedLocation);
+        } else if (visitedLocation.getScope() == Location.Scope.INTERNATIONAL) {
+            visitedListView.getItems().add("[I] " +visitedLocation);
+        }
     }
 
     @FXML
     void locationaryButtonClicked(ActionEvent event) {
         Main.switchScreen("home-view.fxml");
     }
+
+    @FXML
+    void citywideChecked(ActionEvent event) {
+        updateView();
+    }
+
+    @FXML
+    void domesticChecked(ActionEvent event) {
+        updateView();
+    }
+
+    @FXML
+    void internationalChecked(ActionEvent event) {
+        updateView();
+    }
+
+
+    @FXML
+    void updateView() {
+        visitedListView.getItems().clear();
+
+        for (Location location: visited.getLocations()) {
+            if (citywideRadioButton.isSelected()) {
+                if (location.getScope() == Location.Scope.CITYWIDE) {
+                    visitedListView.getItems().add("[C] " + location);
+                }
+            }
+            if (domesticRadioButton.isSelected()) {
+                if (location.getScope() == Location.Scope.DOMESTIC) {
+                    visitedListView.getItems().add("[D] " + location);
+                }
+            }
+            if (internationalRadioButton.isSelected()) {
+                if (location.getScope() == Location.Scope.INTERNATIONAL) {
+                    visitedListView.getItems().add("[I] " + location);
+                }
+            }
+        }
+
+
+
+    }
+
+
+
+
 
 }
